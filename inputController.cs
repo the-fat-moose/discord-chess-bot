679a062930669ea49f33848aa74412b3845165aa
Moose <yugihanmaster321@gmail.com>
2023-07-15 01:08:39 -0400

    Project Setup

---
+++ inputController.cs
@@ -0,0 +1,45 @@
+namespace ChessBot
+{
+    /*class DisplayBoard
+    {
+        private static string[,] chessBoard = new string[8,8];
+
+        public static void Main(string[] args)
+        {
+
+            for(int i = 0; i < 8; i++)
+            {
+                for(int j = 0; j < 8; j++)
+                {
+                    if ((i + j) % 2 == 1)
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkGray;
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                    }
+
+                    if (j != 7)
+                    {
+                        Console.Write("  ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }*/
+
+    class ChessMove
+    {
+        private enum Piece
+        {
+            
+        }
+    }
+}
