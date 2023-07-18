389f35a5e1ed956a9ec85ab33cef9a2e8ab36186
Moose <yugihanmaster321@gmail.com>
2023-07-18 04:42:01 -0400

    Initial Bot Setup
    Initial Bot Code Setup
    
    - Missing Secure method of storing discord token

--- Program.cs
+++ Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using Discord.WebSocket;
+using Discord.Net;
+using dotenv.net;
+public class Program
+{
+
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+    private DiscordSocketClient _client;
+
+    public async Task MainAsync()
+    {
+        _client = new DiscordSocketClient();
+        _client.Log += Log;
+
+        var token = "INSERT SECURE METHOD OF OBTAINING DISCORD TOKEN HERE";
+
+        await _client.LoginAsync(Discord.TokenType.Bot, token);
+        await _client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+
+    private Task Log(Discord.LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
